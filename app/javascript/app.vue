603142320eadd60c32a78cb0b1f8806bd4b4d091
Moe Yang <myang@scramsystems.com>
2019-04-17 13:56:04 -0600

    Store & Route created

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,89 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Home'}" exact>
+            <img class="logo" src="./assets/ibotta-logo.png" />
+            ibotta
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Offer'}" exact>
+              Offer
+            </router-link>
+          </li>
+          <li class="nav-item history">
+            <router-link class="nav-link" to="/history" exact>
+              Offer(s) viewed
+            </router-link>
+            <div class="history-items">
+              {{history.length}}
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+      <main>
+       <router-view />
+      </main>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
-    }
-  }
-}
+  name: 'app',
+};
 </script>
 
+<style>
+body {
+  background:linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+</style>
+
+
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+main {
+  padding: 30px;
+  background-color: white;
+  width: 964px;
+  min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1184px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: blue;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
 }
 </style>
---
+++ app/javascript/components/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="logo" src=""  aria-hidden="true" />
+    </div>
+    <div class="get-started">
+      <router-link to="/build">Get started</router-link> find your first offer!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+.logo {
+  height: 300px;
+}
+.get-started {
+  padding-top: 20px;
+  font-size: 25px;
+}
+</style>
